c480339792924eb6a5b804daada138b7947b44af
SAMSUNG <bluejeansh@hanmail.net>
2023-12-27 15:20:21 +0900

    sub1 브랜치로 push2

--- githubtest/src/main/java/com/example/github/GithubtestApplication.java
+++ githubtest/src/main/java/com/example/github/GithubtestApplication.java
@@ -9,6 +9,7 @@ public class GithubtestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GithubtestApplication.class, args);
 		System.out.println("sub1 브랜치로 push");
+		System.out.println("sub1 브랜치로 push2");
 	}
 
 }
